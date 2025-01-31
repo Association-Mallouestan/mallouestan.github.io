fb2e55603cab547537b585fce8cc05855b5b7187
Cortney <contact@cortneyknorr.com>
2025-01-31 09:13:43 +0000

    Updating sw and ionicons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,37 @@
 async function registerServiceWorker() {
+    const swReleaseNumber = 1;
+
+    const localSwNumber = localStorage.getItem("swReleaseNumber");
+
+    async function installSw(){
+        try {
+            const registration = await navigator.serviceWorker.register(
+            "/sw.js"
+            );
+            localStorage.setItem("swReleaseNumber", swReleaseNumber);
+            console.log(
+            "Service Worker registered with scope:",
+            registration.scope
+            );
+        } catch (error) {
+            console.error("Service Worker registration failed:", error);
+        }
+    }
+
     if ("serviceWorker" in navigator) {
-      try {
-        const registration = await navigator.serviceWorker.register(
-          "/assets/js/sw.js"
-        );
-        console.log(
-          "Service Worker registered with scope:",
-          registration.scope
-        );
-      } catch (error) {
-        console.error("Service Worker registration failed:", error);
-      }
+
+        if(localSwNumber && localSwNumber == swReleaseNumber){
+            console.log(`It would seem that the registered SW matches current`)
+            if((await navigator.serviceWorker.getRegistrations()).length == 0) {
+                console.log("But no service worker running");
+                await installSw();
+            }
+        } else {
+            console.log(`SW number does not match, hence cleanup`);
+            await Promise.all((await navigator.serviceWorker.getRegistrations()).map(sw => sw.unregister()));
+
+            await installSw();
+        }
     }
   }
 
---
+++ sw.js
@@ -0,0 +1,110 @@
+
+const cacheName = "ocv2";
+const cacheAddUrls = [
+  "/id", 
+  "/", 
+  "/404", 
+  "/assets/main.css", 
+  "/images/logo.jpg",
+  "https://unpkg.com/ionicons@4.5.0/dist/fonts/ionicons.woff2?v=4.5.0-3",
+  "https://unpkg.com/ionicons@4.5.0/dist/fonts/ionicons.woff2",
+  "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;900&display=swap",
+  "https://fonts.gstatic.com/s/inter/v18/UcC73FwrK3iLTeHuS_nVMrMxCp50SjIa1ZL7.woff2",
+  "/assets/js/scripts.js",
+  "/assets/js/main.js",
+  "/assets/js/common.js",
+  "https://unpkg.com/ionicons@4.5.0/dist/css/ionicons.min.css",
+  "/images/favicons/android-chrome-192x192.png",
+  "/images/favicons/favicon.ico"
+];
+const cacheExpiry = 300000 //300s
+
+let timestamp = 0;
+
+async function initialCacheLoad(){
+  console.debug("Loading initial data")
+  
+  // console.debug("    Fetching the files listed in sitemap")
+  let response = await fetch("/sitemap.xml").then(t => t.text());
+  const urls = response.matchAll(/<loc>\/(.*)<.loc>/g).map(e => e[1]);
+  const cache = await caches.open(cacheName);
+
+
+  // console.debug(`      Adding ${urls.length} files to the cache`)
+  await Promise.all(urls.map(async (file) => {
+      return cache.add(file);
+  }));
+
+  // console.debug(`      Adding ${cacheAddUrls.length} files to the cache`)
+  await Promise.all(cacheAddUrls.map(async (file) => {
+    return cache.add(file);
+}));
+}
+
+// Installing the Service Worker
+self.addEventListener("install", async (event) => {
+  console.debug("Installing service worker");
+  try {
+    await initialCacheLoad();
+  } catch (error) {
+    console.error("Service Worker installation failed:", error);
+  }
+});
+
+self.addEventListener("fetch", (ev) => {
+  ev.respondWith(staleWhileRevalidate(ev));
+});
+
+async function staleWhileRevalidate(ev) {
+  try {
+    // Return the cache response
+    // Revalidate as well and update cache
+    const [cacheResponse, cache] = await Promise.all([
+      caches.match(ev.request),
+      caches.open(cacheName)
+    ]);
+
+    if(cacheResponse){
+      let cacheDateParent = cacheResponse.clone().headers.entries().find(e => e[0] == "date");
+      if(cacheDateParent){
+        let cacheDate = new Date(cacheDateParent[1]);
+        if(cacheDate.getTime() + cacheExpiry > Date.now()){
+          console.log(`Serving ${ev.request.url} from cache stored at ${cacheDate}`);
+          return cacheResponse;
+        } else {
+          console.log(`Cache has expired for ${ev.request.url}, age: ${(Date.now() - cacheDate.getTime())/1000}`)
+        }
+      }
+    } 
+    try {
+      const fetchResponse = await fetch(ev.request);
+      cache.put(ev.request, fetchResponse.clone())
+        .then(a => console.log(`Storing ${ev.request.url} in cache`));
+      console.log(`Serving ${ev.request.url} from network`)
+      return fetchResponse;
+    } catch (err) {
+      // Most likely no internet
+      if(cacheResponse) {
+        return cacheResponse;
+      } 
+      throw "Nothing to respond";
+    }
+
+
+  } catch (err) {
+    const cache = await caches.open(cacheName);
+    if(/(png|jpg|jpeg)$/i.test(ev.request.url)){
+      console.log(`    Detecting image request so substituting with logo`)
+      return await cache.match("/images/logo.jpg");
+    } else {
+      console.log(err);
+      return await cache.match("/404");
+    }
+  }
+}
+
+self.addEventListener('activate', function(event) {
+    console.log('Claiming control');
+    return self.clients.claim();
+  });
+
