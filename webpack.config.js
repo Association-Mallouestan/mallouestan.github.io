383290e948c2031986a8f62efb504712b28a268d
Cortney <contact@cortneyknorr.com>
2025-05-09 12:20:13 +0200

    Updating webpack for scss file

--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,20 @@ module.exports = {
         scripts: './_tech/assets/js/scripts.js',
         notes: './_tech/assets/js/notes.js',
         main: './_tech/assets/js/main.js',
-        sw: {import: "./_tech/assets/js/sw.js", filename: '../../sw.js'}
+        sw: { import: "./_tech/assets/js/sw.js", filename: '../../sw.js' }
     },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'assets/js'),
         clean: true, // Clean the output directory before emit.
     },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: "./_tech/assets/css/main.scss", to: path.resolve(__dirname, 'assets/main.scss') },
+            ],
+        }),
+    ],
     optimization: {
         splitChunks: {
             chunks: 'all',
