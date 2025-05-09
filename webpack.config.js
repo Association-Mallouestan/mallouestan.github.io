c4c03bfbaf40f8e043db97b0eac26616bfcf7a71
Cortney <contact@cortneyknorr.com>
2025-05-09 11:32:55 +0200

    Added webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+
+
+module.exports = {
+    entry: {
+        index: './_tech/js/common.js',
+        scripts: './_tech/js/scripts.js',
+        notes: './_tech/js/notes.js',
+        main: './_tech/js/main.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'assets/js'),
+        clean: true, // Clean the output directory before emit.
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    }
+};
