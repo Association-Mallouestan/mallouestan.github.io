33076b308ecf29c83760cdccc4ca3ceba886187d
Cortney <contact@cortneyknorr.com>
2025-05-11 22:44:11 +0200

    debugging

--- _tech/assets/js/notes.js
+++ _tech/assets/js/notes.js
@@ -346,7 +346,9 @@ function wrapSelectedText(
         baseNode = baseNode.childNodes[selectionData.path[index]];
       }
       const nodeCursor = baseNode.parentElement.nextSibling;
+      console.log("start", nodeCursor);
       while (nodeCursor.tagName == "code" && nodeCursor.getAttribute("npath").split(",").at(-1) < selectionData.path.at(-1)) {
+        console.log(nodeCursor);
         nodeCursor = nodeCursor.nextSibling;
       }
 
