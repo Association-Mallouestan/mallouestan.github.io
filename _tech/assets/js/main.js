92daff2100602098351cf8cf4391c2a23b07d933
Cortney <contact@cortneyknorr.com>
2026-08-15 18:44:15 +0200

    Adding tab features

--- _tech/assets/js/main.js
+++ _tech/assets/js/main.js
@@ -7,122 +7,147 @@ register();
 // Check if the site is running in standalone mode (PWA) 
 if (window.matchMedia("(display-mode: standalone)").matches) {
     bootstrap();
-} 
-
-
-const {matches:motionOK} = window.matchMedia(
-  '(prefers-reduced-motion: no-preference)'
-)
-
-// grab and stash elements
-const tabgroup     = document.querySelector('snap-tabs')
-const tabsection   = tabgroup.querySelector(':scope > section')
-const tabnav       = tabgroup.querySelector(':scope nav')
-const tabnavitems  = tabnav.querySelectorAll(':scope a')
-const tabindicator = tabgroup.querySelector(':scope .snap-indicator')
-
-/* 
-  shared timeline for .indicator 
-  and nav > a colors */
-const sectionScrollTimeline = new ScrollTimeline({
-  scrollSource: tabsection,
-  orientation: 'inline',
-  fill: 'both',
-})
-
-/*
-  for each nav link
-  - animate color based on the scroll timeline
-  - color is active when its the current index*/
-tabnavitems.forEach(navitem => {
-  navitem.animate({
-      color: [...tabnavitems].map(item => 
-        item === navitem
-          ? `var(--text-active-color)`
-          : `var(--text-color)`)
-    }, {
-      duration: 1000,
-      fill:     'both',
-      timeline: sectionScrollTimeline,
-    }
-  )
-})
-
-if (motionOK) {
-  tabindicator.animate({ 
-      transform: [...tabnavitems].map(({offsetLeft}) => 
-        `translateX(${offsetLeft}px)`),
-      width: [...tabnavitems].map(({offsetWidth}) => 
-        `${offsetWidth}px`)
-    }, {
-      duration: 1000,
-      fill:     'both',
-      timeline: sectionScrollTimeline,
-    }
-  )
 }
 
-const setActiveTab = tabbtn => {
-  tabnav
-    .querySelector(':scope a[active]')
-    .removeAttribute('active')
-  
-  tabbtn.setAttribute('active', '')
-  tabbtn.scrollIntoView()
-}
- 
-const determineActiveTabSection = () => {
-  const i = tabsection.scrollLeft / tabsection.clientWidth
-  const matchingNavItem = tabnavitems[i]
-  
-  matchingNavItem && setActiveTab(matchingNavItem)
-}
 
-tabnav.addEventListener('click', e => {
-  if (e.target.nodeName !== "A") return
-  setActiveTab(e.target)
-})
-
-tabsection.addEventListener('scroll', () => {
-  clearTimeout(tabsection.scrollEndTimer)               
-  tabsection.scrollEndTimer = setTimeout(
-    determineActiveTabSection
-  , 100)
-})
 
 document.addEventListener("DOMContentLoaded", async () => {
 
-    
-  if (location.hash)
-    tabsection.scrollLeft = document
-      .querySelector(location.hash)
-      .offsetLeft
-    
-  determineActiveTabSection()
+    // TABS v ================== v
 
-  let playlistToggle = document.getElementById("playlist__toggle");
-  if(playlistToggle){
-    playlistToggle.addEventListener("click", () => {
-      document.querySelector(".playlist").classList.toggle("out");
-    });
-  }
-    
-  // Manage visited folders and local cache storage 
-  var visitedFolders = JSON.parse(localStorage.getItem("visited-folders")) || {};
-  window.vf = visitedFolders;
+    const { matches: motionOK } = window.matchMedia(
+        '(prefers-reduced-motion: no-preference)'
+    )
 
-    if(/mallouestan.org\/[ft]/.test(window.location.href)){
+    // grab and stash elements
+    const tabgroups = [...document.querySelectorAll('.c-tab-section')];
+
+    tabgroups.forEach(tabgroup => {
+        const tabsection = tabgroup.querySelector(':scope > section')
+        const tabnav = tabgroup.querySelector(':scope nav')
+        const tabheader = tabgroup.querySelector(':scope header')
+        const tabnavitems = tabnav.querySelectorAll(':scope a')
+        const tabindicator = tabgroup.querySelector(':scope .c-tab-section--indicator')
+
+        /* 
+          shared timeline for .indicator 
+          and nav > a colors */
+        const sectionScrollTimeline = new ScrollTimeline({
+            scrollSource: tabsection,
+            orientation: 'inline',
+            fill: 'both',
+        })
+
+        /*
+          for each nav link
+          - animate color based on the scroll timeline
+          - color is active when its the current index*/
+        tabnavitems.forEach(navitem => {
+            navitem.animate({
+                color: [...tabnavitems].map(item =>
+                    item === navitem
+                        ? `var(--text-active-color)`
+                        : `var(--text-color)`)
+            }, {
+                duration: 1000,
+                fill: 'both',
+                timeline: sectionScrollTimeline,
+            }
+            )
+        })
+
+        if (motionOK) {
+            tabindicator.animate({
+                transform: [...tabnavitems].map(({ offsetLeft }) =>
+                    `translateX(${offsetLeft}px)`),
+                width: [...tabnavitems].map(({ offsetWidth }) =>
+                    `${offsetWidth}px`)
+            }, {
+                duration: 1000,
+                fill: 'both',
+                timeline: sectionScrollTimeline,
+            }
+            )
+        }
+
+        const setActiveTab = tabbtn => {
+            tabnav
+                .querySelector(':scope a[active]')?.removeAttribute('active')
+
+            tabbtn.setAttribute('active', '')
+            window.scrollTo({
+                top: tabbtn.getBoundingClientRect().top + window.pageYOffset - 90,
+                behavior: "smooth"
+            });
+        }
+
+        const determineActiveTabSection = () => {
+            const i = tabsection.scrollLeft / tabsection.clientWidth
+            const matchingNavItem = tabnavitems[i]
+
+            matchingNavItem && setActiveTab(matchingNavItem)
+        }
+
+        tabnav.addEventListener('click', e => {
+            if (e.target.nodeName !== "A") return
+
+            history.replaceState(null, "", e.target.href);
+
+            const ttab = document
+                .querySelector(location.hash)
+                .offsetLeft;
+            
+            tabsection.scrollLeft = ttab;
+            tabheader.scrollLeft = ttab;
+
+            setActiveTab(e.target)
+            return e.preventDefault();
+        })
+
+        tabsection.addEventListener('scroll', () => {
+            clearTimeout(tabsection.scrollEndTimer)
+            tabsection.scrollEndTimer = setTimeout(
+                determineActiveTabSection
+                , 100)
+        })
+
+        if (location.hash) {
+            const pottab = tabsection.querySelector(":scope " + location.hash)
+            if(pottab){
+                tabsection.scrollLeft = pottab.offsetLeft
+                tabsection.scrollLeft = pottab.offsetLeft             
+                determineActiveTabSection();
+            }
+        }
+
+    })
+
+    // END TABS ^ ================== ^
+
+
+    let playlistToggle = document.getElementById("playlist__toggle");
+    if (playlistToggle) {
+        playlistToggle.addEventListener("click", () => {
+            document.querySelector(".playlist").classList.toggle("out");
+        });
+    }
+
+    // Manage visited folders and local cache storage 
+    var visitedFolders = JSON.parse(localStorage.getItem("visited-folders")) || {};
+    window.vf = visitedFolders;
+
+    if (/mallouestan.org\/[ft]/.test(window.location.href)) {
         let fav = visitedFolders.hasOwnProperty(window.location.href);
-        
+
         const tagheader = document.querySelector(".c-page-heading .tagheader > p");
 
-        if(tagheader){
+        if (tagheader) {
             tagheader.style.background = fav ? "linear-gradient(90deg, #6D719F 0%, #010053 100%)" : "linear-gradient(90deg, #6D9F71 0%, #015300 100%)";
-            
+
             tagheader.addEventListener("click", () => {
                 fav = !fav;
-    
-                if(fav){
+
+                if (fav) {
                     visitedFolders[window.location.href] = document.title;
                     localStorage.setItem("visited-folders", JSON.stringify(visitedFolders));
                 } else {
@@ -132,18 +157,18 @@ document.addEventListener("DOMContentLoaded", async () => {
                 tagheader.style.background = fav ? "linear-gradient(90deg, #6D719F 0%, #010053 100%)" : "linear-gradient(90deg, #6D9F71 0%, #015300 100%)";
             });
         }
-    
-        if(Object.keys(visitedFolders).length > 0){
+
+        if (Object.keys(visitedFolders).length > 0) {
             var ressourceMenu = [...document.querySelectorAll(".c-header__inner .nav__item.dropdown > span")]
-            .filter(n => /Ressources/.test(n.innerText))[0]
-            .parentElement
-            .querySelector(".dropdown-menu");
-    
+                .filter(n => /Ressources/.test(n.innerText))[0]
+                .parentElement
+                .querySelector(".dropdown-menu");
+
             const hr = document.createElement("hr");
             hr.style.margin = "8px 8px 8px 8px"
             ressourceMenu.appendChild(hr);
-        
-            for(let f in visitedFolders){
+
+            for (let f in visitedFolders) {
                 const link = document.createElement("a");
                 link.classList.add("nav__link");
                 link.href = f;
--- collections/_ressources/2025-04-03-guide-de-visite.md
+++ collections/_ressources/2025-04-03-guide-de-visite.md
@@ -8,83 +8,90 @@ tags:
 image: /images/ressources/bottes.jpg
 author: mallouestan
 
+hide-top: true
+
 content_blocks:
+  - _bookshop_name: hero
+    title: Guide de visite
+    description_md: >-
+      Bienvenue au Mallouestan ! Nous accueillons des visiteuse·eurs toute l'année et nous sommes heureux de vous partager quelques informations pour que votre visite se passe au mieux. Le lieu est ouvert aux visiteuse·eurs, mais nous demandons de nous contacter en avance si possible. Vous trouverez ci-dessous des informations, des conseils et quelques unes des règles du lieu.
+    
+    image: '/images/ressources/bottes.jpg'
+    image_alt: Des bottes pour randonner dans la boue du Mallouestan
+    
+    cta_button: Nous contacter
+    cta_button_link: "#contact"
+
+  - _bookshop_name: tab-section
+    title: Un autre monde
+    description: >-
+      Le Mallouestan est un lieu avec pleins de choses chouettes à découvrir. Lors de ta visite, tu pourras profiter de la nature, d'un collectif solidaire et écologique, de nombreuses activités et de moments de partage. **Mais notre mode de vie est singulier**, de notre expérience, **de nombreuses personnes ont été surprises par l'écart entre notre mode de vie et celui de la société dans laquelle elles vivent**. Les visiteuse·eurs nous parlent souvent de "choc culturelle". Nous préférons donc te prévenir de comment se passe certaines choses au Mallouestan, nous t'invitons à lire les points çi-dessous:
+    tabs:
+      - title: Peu d'électricité
+        content: >-
+          Bla Bla pas d'élec, peu d'écrans, eau chaude
+      - title: Eau et toilettes
+        content: >-
+          Toilette seche partagé et qu'elques points d'eau
+      - title: Campagne
+        content: >-
+          Le village le plus proche est à 25min à pied, n'as pas de supermarché ....
+      - title: Social
+        content: >-
+          Passage, visites, vies collective => dense et donc fatiguante il faut faire attention a sa fatigue
+  
+  - _bookshop_name: description-left
+    title: Comment préparer ta visite
+    description_md: >-
+      Ta visite bla bla bla
+    image: '/images/NEIGE.jpg'
+    image_alt: Le lieu sous la neige
+
   - _bookshop_name: tab-section
     tabs:
-      - title: "Avant de venir"
-        content: >
-          Le Mallouestan est un lieu ouvert aux visiteuse·eurs. 
-          Nous accueillons les visiteurs et visiteuses en dortoir, en tente ou en camion. Nous demandons seulement, quand cela est possible, de nous contacter en avance. Vous trouverez ci-dessous des informations, des conseils et quelques unes des règles du lieu.
-      - title: "Pendant la visite"
-        content: >
-          PLV est un lieu ouvert aux visiteuse·eurs.
+      - title: Ce que tu peux emmener
+        content: >-
+          - Draps et serviettes (si tu n'en n'as pas on peut t'en fournir) 
+          - Des bottes de pluie (on en a quelques-unes en stock)
+          - Des vêtements chauds et imperméables (on est en Normandie quand même)
+          - Lampe de poche ou frontale
+          - Chaussons (en hiver on se pèle les pieds)
+      - title: Ce que tu ne dois pas emmener
+        content: >-
+          Toilette seche partagé et qu'elques points d'eau
+
+  - _bookshop_name: description-center
+    title: Le prix libre
+    description_html: >-
+      Prix libre bla bla bla
+
+  - _bookshop_name: tab-section
+    title: Ce qui est attendue de moi
+    description: >-
+      Bla Bla
+    tabs:
+      - title: A lire avant de venir
+        content: >-
+          Bla Bla charte
+      - title: Autres attentes
+        content: >-
+          - D’avoir lu la charte avant ton arrivée
+          - Le lieu fonctionne à prix libre, tu participes à la hauteur que tu peux et que tu veux
+          - De ne pas représenter une charge de travail pour le groupe
+          - De ne consommer ni drogue ni tabac sur le lieu (il est possible de fumer dans des zones qui te seront indiquées, hors de l'enceinte du lieu)
+          - De ne pas avoir de comportements violents non-légitimes.
+          - Une communication minimale permettant l'organisation collective
+          - Accepter de co-habiter avec des animaux même au sein des habitations (chien•nes, chat•tes)
+
+  - _bookshop_name: tab-section
+    title: Derniers conseils et infos
+    description: >-
+      Bla Bla
+    tabs:    
+      - title: Comment venir
+        content: >-
+          Train le lundi, voiture, stop "guiberville
+      - title: Les repas sur place
+        content: >-
+          La préparons des repas est collective les jours de chantiers (quatre jours par semaine). Bien que nous mangions ensemble par défaut, manger ensemble n’est en rien obligatoire. La cuisine et la nourriture sont en libre accès.
 ---
-
-Le Mallouestan est un lieu ouvert aux visiteuse·eurs. 
-Nous accueillons les visiteurs et visiteuses en dortoir, en tente ou en camion. Nous demandons seulement, quand cela est possible, de nous contacter en avance. Vous trouverez ci-dessous des informations, des conseils et quelques unes des règles du lieu.
-
-
-### Qu’est-ce que je devrais amener ?
-
-- Draps et serviettes (si tu n'en n'as pas on peut t'en fournir) 
-- Des bottes de pluie (on en a quelques-unes en stock)
-- Des vêtements chauds et imperméables (on est en Normandie quand même)
-- Lampe de poche ou frontale
-- Chaussons (en hiver on se pèle les pieds)
-
-![medium](../images/NEIGE.jpg)  
-
-
-### Quels produits alimentaires n’ai-je pas le droit de consommer dans l'enceinte du Mallouestan ?
-
-- Produits d'origine animale (viande, oeufs, lait, miel …)
-- Produits ultra-transformés (biscuits, margarine, sodas …) 
-- L’alcool
-
-![medium](../images/cuisine.jpg) 
-
-Plus de détails sur nos choix alimentaires ici : [choix alimentaires](./choix-alimentaires)  
-
-
-### Quels services sont disponibles sur place ?
-
-#### Douche et toilettes
-- À l’eau chaude (gant de toilette ou bassine + écocup)
-- À l’eau froide (douche ou rivière)
-- Toilettes sèches (et spacieuses) à l’extérieur
-Nous sommes raccordé·es au réseau d'eau potable, mais n'avons pas de chauffe-eau.
-Pour une douche chaude ou une bouillotte, attends le moment de la cuisson du repas et demande à la personne qui s'occupe du feu de faire chauffer de l'eau pour toi.
-
-#### Cuisine et nourriture 
-La préparons des repas est collective les jours de chantiers (quatre jours par semaine). Bien que nous mangions ensemble par défaut, manger ensemble n’est en rien obligatoire. La cuisine et la nourriture sont en libre accès.
-
-#### Internet, réseau et électricité
-- Partage de connexion disponible à certains moments de la journée
-- L’opérateur Orange n’a pas de couverture 
-- La charge des téléphones est limitée à 1 jour sur 2. Pour tout autre équipement électrique, cela peut être négocié en fonction du contexte et du niveau de besoin.
-
-
-### Qu’est-ce qui est attendu de moi ?
-
-- D’avoir lu la charte avant ton arrivée
-- Le lieu fonctionne à prix libre, tu participes à la hauteur que tu peux et que tu veux
-- De ne pas représenter une charge de travail pour le groupe
-- De ne consommer ni drogue ni tabac sur le lieu (il est possible de fumer dans des zones qui te seront indiquées, hors de l'enceinte du lieu)
-- De ne pas avoir de comportements violents non-légitimes.
-- Une communication minimale permettant l'organisation collective
-- Accepter de co-habiter avec des animaux même au sein des habitations (chien•nes, chat•tes)
-
-[Charte du Mallouestan](/r/charte-principale)
-
-![medium](../images/ressources/cozy.jpg)
-
-### Comment je me rends au Mallouestan ?
- 
-Nous pouvons venir te chercher lors d’une de nos sorties en voiture, tous les **lundis jusqu'à 13h30** à l'aire de covoiturage de Torigny les Villes - sortie 40 de l'autoroute A84.
-
-Si tu viens à vélo, nous avons un petit espace de stockage de vélos abrité tout près de l'auberge.
-
-Si tu viens en voiture, nous avons un parking indiqué sur le plan qui suit (indiqué par le carré rouge).
-
-![Planduparking](../images/Planparking.png)
-
